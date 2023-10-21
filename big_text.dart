847138d436b43e32a2566991c5195064e664fba9
nongthangkonsamcha <126575141+nongthangkonsamcha@users.noreply.github.com>
2023-10-22 00:58:58 +0530

    Add files via upload

---
+++ big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+// ignore: must_be_immutable
+class BigText extends StatelessWidget {
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+  BigText(
+      {Key? key,
+        required this.text,
+        this.color = const Color(0xFF332d2B),
+        this.size = 20,
+        this.overflow = TextOverflow.ellipsis})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overflow,
+      style: TextStyle(
+          fontFamily: 'Roboto',
+          color: color,
+          fontSize: size,
+          fontWeight: FontWeight.w400),
+    );
+  }
+}
---
+++ food_page_body.dart
@@ -0,0 +1,93 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+import '../widgets/big_text.dart';
+import '../widgets/small_text.dart';
+
+class FoodPageBody extends StatefulWidget {
+  const FoodPageBody({Key? key}) : super(key: key);
+
+  @override
+  State<FoodPageBody> createState() => _FoodPageBodyState();
+}
+
+class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.redAccent,
+      height: 320,
+      child: PageView.builder(
+          controller: pageController,
+          itemCount: 5,
+          itemBuilder: (context, position) {
+            return _buildPageItem(position);
+          }),
+    );
+  }
+
+  Widget _buildPageItem(int index) {
+    return Stack(children: [
+      Container(
+        height: 220,
+        margin: EdgeInsets.only(left: 10, right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color:
+          index.isEven ? const Color(0xFF69c5df) : const Color(0xFF9294cc),
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage("assets/image/abc.jpeg")),
+        ),
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          height: 120,
+          margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.white,
+          ),
+          child: Container(
+            padding: EdgeInsets.only(top: 10, right: 15, left: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                BigText(text: "Manipur Side"),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Wrap(
+                      children: List.generate(5, (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          )),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    SmallText(text: "4.5"),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    SmallText(text: "1287"),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    SmallText(text: "comments")
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+}
